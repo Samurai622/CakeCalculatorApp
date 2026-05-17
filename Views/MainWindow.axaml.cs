d3f931ccda4a8be9e274f07689c613142f450e03
Samurai622 <vovik13on@gmail.com>
2026-05-17 16:54:54 +0300

    feat(views): implement OpenFileDialog and SaveFileDialog for local DB

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -8,4 +8,36 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private async void OpenFile_Click(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Відкрити базу рецептів",
+            AllowMultiple = false
+        };
+        dialog.Filters.Add(new FileDialogFilter { Name = "JSON Files", Extensions = { "json" } });
+
+        var result = await dialog.ShowAsync(this);
+        if(result != null && result.Length > 0)
+        {
+            Console.WriteLine($"Вибрано файл: {result[0]}");
+        }
+    }
+
+    private async void SaveFile_click(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new SaveFileDialog
+        {
+            Title = "Зберегти базу рецептів",
+            DefaultExtension = "json",
+        };
+        dialog.Filters.Add(new FileDialogFilter { Name = "JSON Files", Extensions = { "json" } });
+
+        var result = await dialog.ShowAsync(this);
+        if(result != null)
+        {
+            Console.WriteLine($"Збережено файл: {result}");
+        }
+    }
 }
